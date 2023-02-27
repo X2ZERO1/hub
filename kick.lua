56d95cd206e33c03eacb17b8db0d4e7317b7c229
X2ZERO1 <118667223+X2ZERO1@users.noreply.github.com>
2023-02-27 03:44:14 -0800

    Update kick.lua

--- kick.lua
+++ kick.lua
@@ -1,2 +1,36 @@
-game.Players.LocalPlayer:kick("Playback X Hub New Version To Discord \n https://discord.gg/xTHsyt7a")
-setclipboard("https://discord.gg/xTHsyt7a")
+if not game:IsLoaded() then
+    local Loaded = Instance.new("Message",workspace)
+    Loaded.Text = 'Wait Game Loading'
+    game.Loaded:Wait()
+    Loaded:Destroy()
+    wait(10)
+end
+
+join = game.Players.localPlayer.Neutral == false
+if (shared.Team == "Pirates" or shared.Team == "Marines") and not join then
+    repeat wait()
+        pcall(function()
+            join = game.Players.localPlayer.Neutral == false
+            if shared.Team == "Pirates" then
+                for i,v in pairs({"MouseButton1Click", "MouseButton1Down", "Activated"}) do
+                    for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton[v])) do
+                        v.Function()
+                    end
+                end
+            elseif shared.Team == "Marines" then
+                for i,v in pairs({"MouseButton1Click", "MouseButton1Down", "Activated"}) do
+                    for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.ViewportFrame.TextButton[v])) do
+                        v.Function()
+                    end
+                end
+            else
+                for i,v in pairs({"MouseButton1Click", "MouseButton1Down", "Activated"}) do
+                    for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton[v])) do
+                        v.Function()
+                    end
+                end
+            end
+        end)
+    until join == true and game.Players.LocalPlayer.Team ~= nil and game:IsLoaded() 
+end
+
